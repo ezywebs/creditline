5f9f77f4bccc79dbe1e9c618f214f9f13c966601
Alex <info@ezwebsupport.com>
2018-11-19 23:34:42 +0000

    ui parts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
+require 'httparty'
+
 class WelcomeController < ApplicationController
   def index
-    credit_lines = HTTParty.get('https://creditline-eng80lvl.c9users.io/api/v1/credit_lines')
+    # response = HTTParty.get('https://creditline-eng80lvl.c9users.io/api/v1/credit_lines')
+    
+    
+    # case response.code
+    #   when 200
+    #     obj = JSON.parse(response.body)
+    #   when 404
+    #   obj = "Not found"
+    #   when 500...600
+    #     puts "ZOMG ERROR #{response.code}"
+    # end
   end
 end
