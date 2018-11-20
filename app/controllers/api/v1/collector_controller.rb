b17127159933ec453c949676261c096df702948a
Alex <info@ezwebsupport.com>
2018-11-20 22:15:19 +0000

    calculation fixes

--- app/controllers/api/v1/collector_controller.rb
+++ app/controllers/api/v1/collector_controller.rb
@@ -1,8 +1,56 @@
 module Api
   module V1
     class CollectorController < ApplicationController
+      # def charge
+      #   credit_line = CreditLine.find(params[:id])
+      #   statement_date = credit_line.last_statement
+      #   statement_ending = statement_date + 30.days
+      #   draws = Draw.where(credit_line_id: params[:id], :created_at => statement_date..statement_ending)
+      #   payments = Payment.where(credit_line_id: params[:id], :created_at => statement_date..statement_ending)
+        
+      #   # change sign of amount to process all transactions together
+      #   payments.each { |p| p.amount = p.amount * -1}
+
+      #   # merging all transactions together and sorting by date
+      #   transactions = draws + payments
+      #   transactions.sort_by! &:created_at
+      
+      #   interest = 0
+      #   balance = 0
+
+      #   transactions.each_with_index do |t, i|
+      #     # finding most recent outstanding balance
+      #     balance = balance + t.amount
+          
+      #     # finding period for new balance to calculate interest
+      #     transaction_beginning = t.created_at
+      #     transaction_ending = transactions[i+1].nil? ? statement_ending : transactions[i+1].created_at 
+      #     period = (transaction_ending.to_date - transaction_beginning.to_date).round
+          
+      #     # calculating interest and adding to previous
+      #     interest = interest + balance * (credit_line.apr / 100) / 365 * period
+      #   end
+      
+      #   # credit_line.update(:last_statement => statement_ending + 1.days)
+      
+      #   render json: {status: "SUCCESS", message: "CollectorController is working", data: {interest: interest.round(2), balance: balance, credit_line: credit_line, transactions: transactions}}, status: :ok
+      # end
+      
+      
       def charge
-        credit_line = CreditLine.find(params[:id])
+        data = calculate_interest(params[:id])
+        CreditLine.find(params[:id]).update(:last_statement => data[:statement_ending] + 1.days, :available => data[:credit_line][:available] - data[:interest])
+        render json: {status: "SUCCESS", message: "Charge interest", data: data}, status: :ok
+      end
+      
+      def view
+        data = calculate_interest(params[:id])
+        render json: {status: "SUCCESS", message: "View statement", data: data}, status: :ok
+      end
+
+      private
+      def calculate_interest(id)
+        credit_line = CreditLine.find(id)
         statement_date = credit_line.last_statement
         statement_ending = statement_date + 30.days
         draws = Draw.where(credit_line_id: params[:id], :created_at => statement_date..statement_ending)
@@ -18,26 +66,25 @@ module Api
         interest = 0
         balance = 0
 
-        transactions.each_with_index do |t, i|
-          # finding most recent outstanding balance
-          balance = balance + t.amount
-          
-          # finding period for new balance to calculate interest
-          transaction_beginning = t.created_at
-          transaction_ending = transactions[i+1].nil? ? statement_ending : transactions[i+1].created_at 
-          period = (transaction_ending.to_date - transaction_beginning.to_date).round
-          
-          # calculating interest and adding to previous
-          interest = interest + balance * (credit_line.apr / 100) / 365 * period
+        if transactions.count.eql?(0) and balance >= 0
+          balance = credit_line.limit - credit_line.available
+          interest =  balance * (credit_line.apr / 100) / 365 * 30
+        else 
+          transactions.each_with_index do |t, i|
+            # finding most recent outstanding balance
+            balance = balance + t.amount
+            
+            # finding period for new balance to calculate interest
+            transaction_beginning = t.created_at
+            transaction_ending = transactions[i+1].nil? ? statement_ending : transactions[i+1].created_at 
+            period = (transaction_ending.to_date - transaction_beginning.to_date).round
+            
+            # calculating interest and adding to previous
+            interest = interest + balance * (credit_line.apr / 100) / 365 * period
+          end
         end
-      
-        credit_line.update(:statement_date => statement_ending + 1.days)
-      
-        render json: {status: "SUCCESS", message: "CollectorController is working", interest: interest.round(2), balance: balance, credit_line: credit_line, transactions: transactions}, status: :ok
-      end
-      
-      def force_charge
         
+        {interest: interest.round(2), balance: balance, credit_line: credit_line, transactions: transactions, statement_ending: statement_ending}
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
       resources :credit_lines
       resources :draws
       resources :payments
-      post "/collector/:id" => "collector#charge"
+      get "/collector/charge/:id" => "collector#charge"
+      get "/collector/:id" => "collector#view"
     end
   end
   
